d626c0cf050ac82c06b817707e1483131f641f57
zeynebyy <zeynepbayram101206@gmail.com>
2024-03-24 18:07:30 +0300

    Dizideki en büyük sayıyı bulma problemi için
    PROJE

---
+++ ÖDEV-1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    // İTERATİF KODU
+    // Verilen bir dizi içindeki en büyük sayıyı bulan bir metod oluşturdum 
+
+    static int EnBuyukSayi(int[] dizi)
+    {
+        int enBuyuk = dizi[0]; // İlk elemanı en büyük olarak kabul edelim 
+                               // Döngü oluşturup  dizinin tüm elemanlarını kontrol ediyoruz. 
+        for (int i = 1; i < dizi.Length; i++)
+        {
+            // Eğer dizinin mevcut elemanı, şu ana kadar bulunan en büyük sayıdan daha büyükse, 
+            // en büyük sayıyı güncelliyoruz 
+            if (dizi[i] > enBuyuk)
+            {
+                enBuyuk = dizi[i]; // Bu satır for döngüsü içinde olmalı
+            }
+        }
+        return enBuyuk;
+    }
+    static void Main(string[] args)
+    {
+        // Örnek bir dizi oluşturuyoruz. 
+        int[] dizi = { 5, 8, 3, 12, 6, 9 };
+        // EnBuyukSayi metodunu çağırarak en büyük sayıyı buluyoruz. 
+        int enBuyuk = EnBuyukSayi(dizi);
+        // Bulunan en büyük sayıyı ekrana yazdırıyoruz. 
+        Console.WriteLine("Dizideki en büyük sayı (iteratif): " + enBuyuk);
+        Console.ReadLine();
+    }
+}
---
+++ ÖDEV-1/RECURSİVE/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    // Dizideki en büyük sayıyı özyinelemeli (recursive) olarak bulan bir metod tanımladık. 
+    static int EnBuyukSayiyiBul(int[] dizi, int n)
+    {
+        // Dizinin boyutu 1 ise, dizinin tek elemanı en büyük sayıdır, dolayısıyla bu elemanı döndürürüz. 
+        if (n == 1)
+            return dizi[0];
+
+        // "Math.Max" fonksiyonu, iki sayı arasından büyük olanı döndüren matematiksel bir fonksiyondur  
+        // Burada, dizinin son elemanı ile (dizi[n - 1]) bir önceki en büyük sayıyı karşılaştırıyoruz 
+        // en büyük olanı belirleriz ve recursive olarak bu işlemi devam ettiririz. 
+        return Math.Max(dizi[n - 1], EnBuyukSayiyiBul(dizi, n - 1));
+    }
+
+    static void Main(string[] args)
+    {
+        // Örnek bir dizi oluşturulur. 
+        int[] dizi = { 5, 8, 3, 12, 6, 9 };
+
+        // EnBuyukSayiyiBul metodu çağırılarak en büyük sayı bulunur. 
+        // Metoda, dizi ve dizinin uzunluğu (n) parametre olarak verilir. 
+        int enBuyuk = EnBuyukSayiyiBul(dizi, dizi.Length);
+
+        // Bulunan en büyük sayı konsola yazdırılır. 
+        Console.WriteLine("Dizideki en büyük sayı (recursive): " + enBuyuk);
+
+        // Kullanıcının konsolu kapatmadan önce bir tuşa basmasını beklemek için kullanılır. 
+        Console.ReadLine();
+    }
+}
