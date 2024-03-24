3c665113346edaa03c1abaac9e96f2d47dbea552
zeynebyy <zeynepbayram101206@gmail.com>
2024-03-24 18:00:53 +0300

    Bir sayının faktöriyelini bulma problemi için;
    PROJE

---
+++ ÖDEV5/fakröriyel_iteratif/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Faktöriyelini bulmak istediğiniz sayıyı girin:");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+
+        int faktoriyel = FaktoriyelHesapla(sayi);
+
+        Console.WriteLine($"{sayi} sayısının faktöriyeli: {faktoriyel}");
+        Console.ReadLine();
+    }
+
+    static int FaktoriyelHesapla(int n)
+    {
+        int faktoriyel = 1;
+        // 1'den n'e kadar olan tüm sayıları çarpıyoruz
+        for (int i = 1; i <= n; i++)
+        {
+            faktoriyel *= i;
+        }
+        return faktoriyel;
+        
+    }
+}
---
+++ ÖDEV5/faktöriyel_recursive/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Faktöriyelini bulmak istediğiniz sayıyı girin:");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+
+        int faktoriyel = FaktoriyelHesapla(sayi);
+
+        Console.WriteLine($"{sayi} sayısının faktöriyeli: {faktoriyel}");
+        Console.ReadLine();
+    }
+
+    static int FaktoriyelHesapla(int n)
+    {
+        // Recursive metot, n'nin 0 veya 1 olduğu durumları ele alır.
+        // Eğer n 0 veya 1 ise, faktöriyel değeri 1 olarak döner.
+        if (n == 0 || n == 1)
+            return 1;
+        // Eğer n 2 veya daha büyük ise, faktöriyel değeri n * FaktoriyelHesapla(n - 1) olarak hesaplanır.
+        // Bu durum recursive olarak n'yi 1 azaltarak devam eder, n = 1 olduğunda base case'e ulaşılır ve recursive işlem sonlanır.
+        else
+            return n * FaktoriyelHesapla(n - 1);
+    }
+}
+
+
